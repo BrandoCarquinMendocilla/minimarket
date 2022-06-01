0282a275a0694f58566b54338f7e555b453932a8
Sebastian Navarro Sam <95194235+Sebkill112@users.noreply.github.com>
2022-05-31 22:17:20 -0500

    Consulta Proveedor avanze

--- src/main/java/gui/FrmConsultaProveedor.java
+++ src/main/java/gui/FrmConsultaProveedor.java
@@ -1,11 +1,27 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
-import javax.swing.JInternalFrame;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
-public class FrmConsultaProveedor extends JInternalFrame {
+import entidad.Proveedor;
+import model.ProveedorModel;
+
+public class FrmConsultaProveedor extends JInternalFrame implements ActionListener {
+	private JTextField txtFiltro;
+	private JTable table;
+	private JButton btnConsultar;
 
 	/**
 	 * Launch the application.
@@ -31,8 +47,60 @@ public class FrmConsultaProveedor extends JInternalFrame {
 		setIconifiable(true);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setTitle("Consulta de Proveedor\r\n");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 618, 340);
+		getContentPane().setLayout(null);
+
+		btnConsultar = new JButton("CONSULTAR\r\n");
+		btnConsultar.addActionListener(this);
+		btnConsultar.setFont(new Font("Arial Black", Font.BOLD, 14));
+		btnConsultar.setBounds(403, 34, 143, 23);
+		getContentPane().add(btnConsultar);
+
+		txtFiltro = new JTextField();
+		txtFiltro.setBounds(168, 37, 204, 20);
+		getContentPane().add(txtFiltro);
+		txtFiltro.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Nombre de Proveedor\r\n");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel.setBounds(10, 40, 137, 14);
+		getContentPane().add(lblNewLabel);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 98, 582, 201);
+		getContentPane().add(scrollPane);
+
+		table = new JTable();
+		table.setModel(new DefaultTableModel(new Object[][] {}, new String[] { "ID", "Nombre", "RUC", "Producto",
+				"Direcci\u00F3n", "Correo", "Pa\u00EDs", "Tel\u00E9fono" }));
+		scrollPane.setViewportView(table);
 
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnConsultar) {
+			actionPerformedBtnConsultarJButton(e);
+		}
+	}
+
+	protected void actionPerformedBtnConsultarJButton(ActionEvent e) {
+		String filtro = txtFiltro.getText().trim();
+
+		// 1 Se limpia los datos del jtable
+		DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+		dtm.setRowCount(0);
+
+		// 2 Se obtiene los campeonatos de la BD
+		ProveedorModel model = new ProveedorModel();
+		List<Proveedor> lista = model.listaProveedorPorNombre(filtro);
+		
+		//3 Se muestran los campeonatos en el JTABLE
+		
+		for(Proveedor x: lista) {
+			Object[] fila = { x.getIdProveedor(),x.getNombre(),x.getRuc(),x.getProducto(),x.getDireccion(),
+					x.getCorreo(),x.getPais(),x.getTelefono()};
+			dtm.addRow(fila);
+		}
+
+	}
 }
--- src/main/java/model/ProveedorModel.java
+++ src/main/java/model/ProveedorModel.java
@@ -11,190 +11,232 @@ import entidad.Proveedor;
 import util.MySqlDBConexion;
 
 public class ProveedorModel {
-	
+
 	private static Logger log = Logger.getLogger(ProveedorModel.class.getName());
-	
+
 	// Registra Proveedor
 
-		public int insertaProveedor(Proveedor obj) {
+	public int insertaProveedor(Proveedor obj) {
 
-			int salida = -1;
+		int salida = -1;
 
-			Connection conn = null;
-			PreparedStatement pstm = null;
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+
+			// Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+
+			// Se prepara el sql
+
+			String sql = "insert into proveedor value(null,?,?,?,?,?,?,?,curtime())";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, obj.getNombre());
+			pstm.setString(2, obj.getRuc());
+			pstm.setString(3, obj.getProducto());
+			pstm.setString(4, obj.getDireccion());
+			pstm.setString(5, obj.getCorreo());
+			pstm.setString(6, obj.getPais());
+			pstm.setString(7, obj.getTelefono());
+
+			log.info(">>> " + pstm);
+
+			// 3 Ejecutamos a la base de datos
+			// Retorna la cantidad de registrados en salida
+			salida = pstm.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
 
-				// Se crea la conexion
-				conn = MySqlDBConexion.getConexion();
-
-				// Se prepara el sql
-
-				String sql = "insert into proveedor value(null,?,?,?,?,?,?,?,curtime())";
-				pstm = conn.prepareStatement(sql);
-				pstm.setString(1, obj.getNombre());
-				pstm.setString(2, obj.getRuc());
-				pstm.setString(3, obj.getProducto());
-				pstm.setString(4, obj.getDireccion());
-				pstm.setString(5, obj.getCorreo());
-				pstm.setString(6, obj.getPais());
-				pstm.setString(7, obj.getTelefono());
-				
-
-				log.info(">>> " + pstm);
-
-				// 3 Ejecutamos a la base de datos
-				// Retorna la cantidad de registrados en salida
-				salida = pstm.executeUpdate();
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstm != null)
-						pstm.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e2) {
-
-				}
 			}
-			log.info(">>> Fin >>> insertaProveedor");
-			return salida;
 		}
-		
-		// Elimina Proveedor
+		log.info(">>> Fin >>> insertaProveedor");
+		return salida;
+	}
 
-		public int EliminaProveedor(int idProveedor) {
+	// Elimina Proveedor
 
-			int salida = -1;
+	public int EliminaProveedor(int idProveedor) {
 
-			Connection conn = null;
-			PreparedStatement pstm = null;
+		int salida = -1;
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+
+			// Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+
+			// Se prepara el sql
+
+			String sql = "delete from proveedor where idProveedor=?";
+			pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, idProveedor);
+
+			log.info(">>> " + pstm);
+
+			// 3 Ejecutamos a la base de datos
+
+			salida = pstm.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
 
-				// Se crea la conexion
-				conn = MySqlDBConexion.getConexion();
-
-				// Se prepara el sql
-
-				String sql = "delete from proveedor where idProveedor=?";
-				pstm = conn.prepareStatement(sql);
-				pstm.setInt(1, idProveedor);
-
-				log.info(">>> " + pstm);
-
-				// 3 Ejecutamos a la base de datos
-
-				salida = pstm.executeUpdate();
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstm != null)
-						pstm.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e2) {
-
-				}
 			}
-
-			return salida;
 		}
-		
-		// Actualiza Proveedor
 
-		public int ActualizaProveedor(Proveedor pro) {
+		return salida;
+	}
 
-			int salida = -1;
+	// Actualiza Proveedor
 
-			Connection conn = null;
-			PreparedStatement pstm = null;
+	public int ActualizaProveedor(Proveedor pro) {
+
+		int salida = -1;
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+
+			// Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+
+			// Se prepara el sql
+
+			String sql = "update proveedor set nombre=?, ruc=?, producto=?, direccion=?, correo=?, pais=?, telefono=? where idProveedor=?";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, pro.getNombre());
+			pstm.setString(2, pro.getRuc());
+			pstm.setString(3, pro.getProducto());
+			pstm.setString(4, pro.getDireccion());
+			pstm.setString(5, pro.getCorreo());
+			pstm.setString(6, pro.getPais());
+			pstm.setString(7, pro.getTelefono());
+			pstm.setInt(8, pro.getIdProveedor());
+
+			log.info(">>> " + pstm);
+
+			// 3 Ejecutamos a la base de datos
+			// Retorna la cantidad de registrados en salida
+			salida = pstm.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
 
-				// Se crea la conexion
-				conn = MySqlDBConexion.getConexion();
-
-				// Se prepara el sql
-
-				String sql = "update proveedor set nombre=?, ruc=?, producto=?, direccion=?, correo=?, pais=?, telefono=? where idProveedor=?";
-				pstm = conn.prepareStatement(sql);
-				pstm.setString(1, pro.getNombre());
-				pstm.setString(2, pro.getRuc());
-				pstm.setString(3, pro.getProducto());
-				pstm.setString(4, pro.getDireccion());
-				pstm.setString(5, pro.getCorreo());
-				pstm.setString(6, pro.getPais());
-				pstm.setString(7, pro.getTelefono());
-				pstm.setInt(8, pro.getIdProveedor());
-
-				log.info(">>> " + pstm);
-
-				// 3 Ejecutamos a la base de datos
-				// Retorna la cantidad de registrados en salida
-				salida = pstm.executeUpdate();
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstm != null)
-						pstm.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e2) {
-
-				}
 			}
-
-			return salida;
 		}
-		
-		//Listar Proveedor
-		
-		public List<Proveedor> listaProveedor() {
-			ArrayList<Proveedor> salida = new ArrayList<Proveedor>();
+
+		return salida;
+	}
+
+	// Listar Proveedor
+
+	public List<Proveedor> listaProveedor() {
+		ArrayList<Proveedor> salida = new ArrayList<Proveedor>();
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		try {
+			// Se crea la conexcion
+			conn = MySqlDBConexion.getConexion();
+
+			// Se prepara el sql
+			String sql = "select * from proveedor";
+			pstm = conn.prepareStatement(sql);
+
+			rs = pstm.executeQuery();
+			Proveedor obj = null;
+			while (rs.next()) {
+				obj = new Proveedor();
+				obj.setIdProveedor(rs.getInt(1));
+				obj.setNombre(rs.getString(2));
+				obj.setRuc(rs.getString(3));
+				obj.setProducto(rs.getString(4));
+				obj.setDireccion(rs.getString(5));
+				obj.setCorreo(rs.getString(6));
+				obj.setPais(rs.getString(7));
+				obj.setTelefono(rs.getString(8));
+				salida.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
+		return salida;
+	}
+
+	public List<Proveedor> listaProveedorPorNombre(String filtro) {
+		ArrayList<Proveedor> salida = new ArrayList<Proveedor>();
+
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = MySqlDBConexion.getConexion();
+
+			String sql = "select * from proveedor where nombre like ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, filtro + "%");
 			
-			Connection conn= null;
-			PreparedStatement pstm= null;
-			ResultSet rs = null;
-			try {
-				//Se crea la conexcion
-				conn = MySqlDBConexion.getConexion();
+			log.info(">>> " + psmt);
+			
+			rs = psmt.executeQuery();
+			Proveedor obj = null;
+			while(rs.next()) {
+				obj = new Proveedor();
+				obj.setIdProveedor(1);
+				obj.setNombre(rs.getString(2));
+				obj.setRuc(rs.getString(3));
+				obj.setProducto(rs.getString(4));
+				obj.setDireccion(rs.getString(5));
+				obj.setCorreo(rs.getString(6));
+				obj.setPais(rs.getString(7));
+				obj.setTelefono(rs.getString(8));
+				salida.add(obj);
 				
-				//Se prepara el sql
-				String sql = "select * from proveedor";
-				pstm = conn.prepareStatement(sql);
-				
-				rs = pstm.executeQuery();
-				Proveedor obj = null;
-				while(rs.next()) {
-					obj= new Proveedor();
-					obj.setIdProveedor(rs.getInt(1));
-					obj.setNombre(rs.getString(2));
-					obj.setRuc(rs.getString(3));
-					obj.setProducto(rs.getString(4));
-					obj.setDireccion(rs.getString(5));
-					obj.setCorreo(rs.getString(6));
-					obj.setPais(rs.getString(7));
-					obj.setTelefono(rs.getString(8));
-					salida.add(obj);
-				}
-			}catch (Exception e) {
-				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();		
 			}finally {
 				try {
-					if (pstm!=null) 
-						pstm.close();
-					if (conn!=null) 
-						conn.close();	
-				} catch (Exception e2) {
-					// TODO: handle exception
-				}
+					if (rs != null) rs.close();
+					if (psmt != null) psmt.close();
+					if (conn != null) conn.close();
+				} catch (Exception e2) {}
 			}
-			return salida;
-		}
 
+		return salida;
+	}
 
 }
