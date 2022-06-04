c91eafb48cd9531943061cf57dd5904180e217ea
Michelj1 <105763222+Michelj1@users.noreply.github.com>
2022-06-04 13:12:23 -0500

    Consulta - Empleado

--- src/main/java/gui/FrmConsultaEmpleado.java
+++ src/main/java/gui/FrmConsultaEmpleado.java
@@ -4,8 +4,17 @@ import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class FrmConsultaEmpleado extends JInternalFrame {
+	private JTextField txtNombre;
+	private JTable miTabla;
 
 	/**
 	 * Launch the application.
@@ -31,8 +40,33 @@ public class FrmConsultaEmpleado extends JInternalFrame {
 		setClosable(true);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setTitle("Consulta de Vendedor\r\n");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 850, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar Empleado");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(10, 10, 178, 13);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblIngreseNom = new JLabel("Ingrese Nombre : ");
+		lblIngreseNom.setBounds(10, 45, 99, 13);
+		getContentPane().add(lblIngreseNom);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(119, 42, 197, 19);
+		getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 82, 818, 179);
+		getContentPane().add(scrollPane);
+		
+		miTabla = new JTable();
+		miTabla.setModel(new DefaultTableModel(
+				new Object[][] {},
+				new String[] {"ID", "Nombre", "Apellido", "DNI", "Teléfono", "Correo","Categoria"}
+			));
+		scrollPane.setViewportView(miTabla);
 
 	}
-
 }
