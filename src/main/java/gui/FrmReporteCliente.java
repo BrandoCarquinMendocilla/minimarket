a8e0f6b424ea49ec76bd9f7942445784d25953ff
Emanuel <102432747+Minorbutmajor10@users.noreply.github.com>
2022-06-17 20:27:43 -0500

    AVANCE CLIENE

--- src/main/java/gui/FrmReporteCliente.java
+++ src/main/java/gui/FrmReporteCliente.java
@@ -1,12 +1,32 @@
 package gui;
 
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
-public class FrmReporteCliente extends JInternalFrame {
+import entidad.Cliente;
+import model.ClienteModel;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.swing.JRViewer;
+import util.GeneradorReporte;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class FrmReporteCliente extends JInternalFrame implements ActionListener {
+	private JButton btnCONSULTAR;
+	private JPanel panel;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -32,8 +52,46 @@ public class FrmReporteCliente extends JInternalFrame {
 		setClosable(true);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setTitle("Reporte de Clientes");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 886, 636);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Reporte de Clientes");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 30));
+		lblNewLabel.setBounds(303, 22, 421, 52);
+		getContentPane().add(lblNewLabel);
+		
+		btnCONSULTAR = new JButton("CONSULTAR");
+		btnCONSULTAR.addActionListener(this);
+		btnCONSULTAR.setFont(new Font("Arial", Font.ITALIC, 20));
+		btnCONSULTAR.setBounds(10, 173, 854, 31);
+		getContentPane().add(btnCONSULTAR);
+		
+		panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Reportes Clientes", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 214, 854, 383);
+		getContentPane().add(panel);
+		panel.setLayout(new BorderLayout());
 
 	}
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCONSULTAR) {
+			do_btnNewButton_actionPerformed(e);
+		}
+	}
+	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+		
+		
+		ClienteModel model =new ClienteModel();
+		List<Cliente>lstCliente= model.listaCliente();
+		
+		JRBeanCollectionDataSource dataSource = new  JRBeanCollectionDataSource(lstCliente);
+		String jasper= "ReporteCliente.jasper";
+		JasperPrint print = GeneradorReporte.genera(jasper, dataSource, null);
+		JRViewer jRViewer = new JRViewer(print);
+		
+		panel.removeAll();
+		panel.add(jRViewer);
+		panel.repaint();
+		panel.revalidate();
+	}
 }
